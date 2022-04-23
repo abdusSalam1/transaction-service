d053f173d0d344798ea1d185bd9dc121e20957db
abdussalam <abdus.salam@teamapt.com>
2022-04-23 23:52:56 +0500

    added coverate

---
+++ src/test/java/com/transaction/deserialzier/AccountCreationTopicDeserializerTest.java
@@ -0,0 +1,34 @@
+package com.transaction.deserialzier;
+
+import com.transaction.domain.Account;
+import org.json.simple.JSONObject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.charset.StandardCharsets;
+
+class AccountCreationTopicDeserializerTest {
+
+    private AccountCreationTopicDeserializer deserializer;
+
+    @BeforeEach
+    public void setup() {
+        this.deserializer = new AccountCreationTopicDeserializer();
+    }
+
+    @Test
+    public void givenAccountBytesWhenDeserializationShouldReturnAccount() {
+        JSONObject obj = new JSONObject();
+        obj.put("id", 123);
+        obj.put("name", "abdus");
+        obj.put("email", "abdus@gmail.com");
+        byte[] account = obj.toString().getBytes(StandardCharsets.UTF_8);
+        Account deserializedAccount = deserializer.deserialize("", account);
+        Assertions.assertNotNull(deserializedAccount);
+        Assertions.assertEquals(123, deserializedAccount.getId());
+        Assertions.assertEquals("abdus", deserializedAccount.getName());
+        Assertions.assertEquals("abdus@gmail.com", deserializedAccount.getEmail());
+    }
+
+}
