05bbbf754514b6101b8817be93d0c8cfeab98d33
abdussalam <abdus.salam@teamapt.com>
2022-04-25 12:04:58 +0500

    fixed transformer

--- src/test/java/com/transaction/transformer/TransactionTransformerTest.java
+++ src/test/java/com/transaction/transformer/TransactionTransformerTest.java
@@ -1,12 +1,14 @@
 package com.transaction.transformer;
 
 import com.transaction.domain.Transaction;
+import com.transaction.enums.TransactionType;
 import com.transaction.model.TransactionModel;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 class TransactionTransformerTest {
 
@@ -30,11 +32,17 @@ class TransactionTransformerTest {
                 .amount(new BigDecimal(200))
                 .description("added for test")
                 .purpose("bought a new phone")
+                .creditAccount("1234")
+                .transactionType(TransactionType.DEBIT)
                 .build();
         Transaction entity = transactionTransformer.toEntity(model);
         Assertions.assertNotNull(entity);
         Assertions.assertEquals("added for test", entity.getDescription());
         Assertions.assertEquals("bought a new phone", entity.getPurpose());
+        Assertions.assertEquals("1234", entity.getCreditAccount());
+        Assertions.assertEquals(TransactionType.DEBIT, entity.getType());
+        Assertions.assertNotNull(entity.getReference());
+        Assertions.assertNotNull(entity.getTransactionDate());
     }
 
     @Test
@@ -42,6 +50,26 @@ class TransactionTransformerTest {
         Transaction entity = null;
         TransactionModel model = transactionTransformer.toModel(entity);
         Assertions.assertNull(model);
+    }
+
+    @Test
+    public void whenEntityGivenShouldReturnModel() {
+        Transaction entity = Transaction.builder()
+                .amount(new BigDecimal(200))
+                .description("added for test")
+                .purpose("bought a new phone")
+                .creditAccount("1234")
+                .type(TransactionType.DEBIT)
+                .reference("123")
+                .transactionDate(LocalDateTime.now())
+                .id(1L)
+                .build();
+        TransactionModel model = transactionTransformer.toModel(entity);
+        Assertions.assertNotNull(model);
+        Assertions.assertEquals("added for test", model.getDescription());
+        Assertions.assertEquals("bought a new phone", model.getPurpose());
+        Assertions.assertEquals("1234", model.getCreditAccount());
+        Assertions.assertEquals(TransactionType.DEBIT, model.getTransactionType());
     }
 
 }
